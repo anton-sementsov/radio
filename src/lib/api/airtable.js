473bc377fe5d38f234c475e9c3d30cbef24c52ee
Anton Sementsov <anton.sementsov@agenatech.com>
2022-03-25 12:58:39 +0200

    fix: import airtable

--- src/lib/api/airtable.js
+++ src/lib/api/airtable.js
@@ -1,3 +1,4 @@
+import Airtable from 'Airtable';
 import { AIRTABLE_API_KEY, AIRTABLE_BASE_KEY } from './constants';
 
 export const airtableDB = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_KEY);
