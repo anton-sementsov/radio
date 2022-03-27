1eddbfad955769d7a7cbac81f7ffed1f31037cf3
Anton Sementsov <anton.sementsov@agenatech.com>
2022-03-28 01:16:04 +0300

    feat: socaial share

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,24 @@ import { Button } from '../components/Button'
 import logo from '../assets/images/logo.png'
 import { ProgressBar } from '../features/home/components/ProgressBar'
 import { CommentStripe } from '../features/home/components/CommentStripe'
+import { FacebookShareButton, TwitterShareButton, FacebookIcon, TwitterIcon } from "react-share";
 import styles from '../styles/Home.module.scss'
+import { SEO } from '../utils/seo';
 
 export default function Home() {
+
+  const seo = {
+    title: 'Grains of Peace on 20ft Radio',
+    content: 'We believe that music is an instrument of peace, a medicine, which power can be felt by everyone and can help to find faith, hope and peace in this world again.',
+    image: '../assets/images/social.jpeg',
+    url: 'https://www.20ftradio.net/grains-of-peace',
+  }
+
   return (
     <div >
       <Head>
         <title>Grains of peace</title>
+        <SEO seo={seo} />
       </Head>
 
       <div className={styles.wrapper}>
@@ -34,7 +45,7 @@ export default function Home() {
             <p >
               {'Listen to music and stand with Ukraine.🇺🇦'.toUpperCase()} <br />
             </p>
-            <p style={{ paddingTop: '20px', maxWidth: '620px', fontFamily : 'ScaniaSansBold', color: '#ffffff' }}>
+            <p style={{ paddingTop: '20px', maxWidth: '620px', fontFamily: 'ScaniaSansBold', color: '#ffffff' }}>
               {'Our goal is $100 000 for humanitarian needs: this funds will be transfered to NBU Fundraising Account for Humanitarian Assistance, Destroyed Property and Infrastructure Restoration Fund and friends-volunteers who are fighting back the occupier in different Ukrainian cities.'.toUpperCase()} <br />
             </p>
             <div style={{ marginTop: '35px' }}>
@@ -43,11 +54,40 @@ export default function Home() {
             <div style={{ marginTop: '20px' }}>
               <Button label='DONATE' onClick={() => { window.open('https://www.paypal.com/donate/?hosted_button_id=FARF8CJJPJMQS', '_blank').focus(); }} />
             </div>
+            <p style={{ marginTop: '15px', fontFamily: 'ScaniaSansBold' }}>
+              SHARE:
+            </p>
+
+            <div style={{ paddingTop: '10px' }}>
+              <span style={{ paddingRight: '10px' }}>
+                <FacebookShareButton
+                  url={seo.url}
+                  quote={seo.title}
+                  hashtag={"#grains-of-peace"}
+                  description={seo.description}
+                  className="Demo__some-network__share-button"
+                >
+                  <FacebookIcon size={32} round />
+                </FacebookShareButton>
+              </span>
+
+              <TwitterShareButton
+                title={seo.title}
+                url={seo.url}
+                hashtags={["#grains-of-peace"]}
+              >
+                <TwitterIcon size={32} round />
+              </TwitterShareButton>
+            </div>
+
+
             <p style={{ marginTop: '15px' }}>
               WANT TO PARTICIPATE? SEND YOUR
               <br />
               PAECEFULL MiX TO  <a style={{ color: '#FECE4D' }} href="mailto:20FTRADIO@GMAIL.COM">20FTRADIO@GMAIL.COM</a>
             </p>
+
+
           </div>
           <div className={styles.timeline}>
             <Timeline />
