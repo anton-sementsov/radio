d5de595c263b9232c629fd81974428bdd2b3fcfe
Anton Sementsov <anton.sementsov@agenatech.com>
2022-03-28 01:21:43 +0300

    feat: add social post image

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   const seo = {
     title: 'Grains of Peace on 20ft Radio',
     content: 'We believe that music is an instrument of peace, a medicine, which power can be felt by everyone and can help to find faith, hope and peace in this world again.',
-    image: '../assets/images/social.jpeg',
+    image: '../assets/images/social2.jpeg',
     url: 'https://www.20ftradio.net/grains-of-peace',
   }
 
